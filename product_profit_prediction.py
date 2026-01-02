2d5d069fdad58c0d0ed0af8dfd53cac37d3e2aee
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-02 19:47:32 +0530

    created data

--- product_profit_prediction.py
+++ product_profit_prediction.py
@@ -7,3 +7,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+
+data = {
+    'Products_Sold': [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
+    'Profit': [5000, 12000, 18000, 25000, 31000, 38000, 45000, 52000, 60000, 68000]
+}
+
+df = pd.DataFrame(data)
