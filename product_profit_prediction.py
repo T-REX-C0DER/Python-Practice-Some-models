d4fd6a6408bd37052391819cda6be23d9c81b963
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-08 13:58:02 +0530

    imroved the comments

--- product_profit_prediction.py
+++ product_profit_prediction.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+#added data manually , data is storeed in key value pairs
 data = {
     'Products_Sold': [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
     'Profit': [5000, 12000, 18000, 25000, 31000, 38000, 45000, 52000, 60000, 68000]
@@ -29,6 +30,7 @@ mse = mean_squared_error(Y_test, Y_pred)
 print("Mean Squared Error:", mse)
 r2 = r2_score(Y_test, Y_pred)
 print("R^2 Score:", r2)
+
 # Visualization
 X_1D = X['Products_Sold'].values
 Y_pred_Full = model.predict(X)
