605e93ffb4a42090177206eedb626e95f9a9ae26
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-23 21:30:46 +0530

    Commented

--- Practice.py
+++ Practice.py
@@ -504,53 +504,53 @@
 # print(result2)
 
 
-# Method Overriding in Python
-class Animal:
-    def sound(self):
-        print("Animal makes a sound")
-class Dog(Animal):
-    def sound(self):
-        print("Dog barks")
-class Cat(Animal):
-    def sound(self):
-        print("Cat meows")
-dog1 = Dog()
-cat1 = Cat()
-dog1.sound()
-cat1.sound()
+# # Method Overriding in Python
+# class Animal:
+#     def sound(self):
+#         print("Animal makes a sound")
+# class Dog(Animal):
+#     def sound(self):
+#         print("Dog barks")
+# class Cat(Animal):
+#     def sound(self):
+#         print("Cat meows")
+# dog1 = Dog()
+# cat1 = Cat()
+# dog1.sound()
+# cat1.sound()
 
-# Magic/Dunder Methods in Python
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-    def __str__(self):
-        return f"Point({self.x}, {self.y})"
-point1 = Point(1, 2)
-point2 = Point(3, 4)
-result = point1 + point2
-print(result)
+# # Magic/Dunder Methods in Python
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
+#     def __str__(self):
+#         return f"Point({self.x}, {self.y})"
+# point1 = Point(1, 2)
+# point2 = Point(3, 4)
+# result = point1 + point2
+# print(result)
 
-# Operator Overloading in Python
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y  
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-    def __mul__(self, other):
-        return Point(self.x * other.x, self.y * other.y)    
-    def __str__(self):
-        return f"Point({self.x}, {self.y})"
-point1 = Point(1, 2)
-point2 = Point(3, 4)
-result = point1 + point2
-print(result)
-result = point1 - point2
-print(result)
-result = point1 * point2
-print(result)
+# # Operator Overloading in Python
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y  
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
+#     def __sub__(self, other):
+#         return Point(self.x - other.x, self.y - other.y)
+#     def __mul__(self, other):
+#         return Point(self.x * other.x, self.y * other.y)    
+#     def __str__(self):
+#         return f"Point({self.x}, {self.y})"
+# point1 = Point(1, 2)
+# point2 = Point(3, 4)
+# result = point1 + point2
+# print(result)
+# result = point1 - point2
+# print(result)
+# result = point1 * point2
+# print(result)
