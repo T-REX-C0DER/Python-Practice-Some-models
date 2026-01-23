5cabe74d07b69b5b179c1d42bab7a4a0ce8210f6
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-23 21:29:27 +0530

    Magic / Dunter methods

--- Practice.py
+++ Practice.py
@@ -518,3 +518,17 @@ dog1 = Dog()
 cat1 = Cat()
 dog1.sound()
 cat1.sound()
+
+# Magic/Dunder Methods in Python
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+point1 = Point(1, 2)
+point2 = Point(3, 4)
+result = point1 + point2
+print(result)
