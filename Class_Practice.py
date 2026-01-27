d53466713bfa1edd00825c818efe0646fd27204c
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-27 15:29:33 +0530

    Added first code

--- Class_Practice.py
+++ Class_Practice.py
@@ -5,3 +5,15 @@
 # student_password = "123456"
 # print(f'Student Name: {student_name} and his rollno is {student_rollno} , aashar no is {student_aadharno}, email is {student_email} and password is {student_password} ')
 
+# student_name = input("Enter the name of student :")
+# student_rollno = int(input("Enter the name of rollno :"))
+# student_aadharno = input("Enter the name of aadharno :")
+# student_email = input("Enter the name of email :")
+# student_password = input("Enter the name of password :")
+# print(f"Student Name: {student_name} and his rollno is {student_rollno} , aashar no is {student_aadharno}, email is {student_email} and password is {student_password} ")
+
+# student_subjects = input("Enter Student Subjects")
+# student_grades1 = int(input("Enter Student Grades"))
+# student_grades2 = int(input("Enter Student Grades"))
+# student_grades3 = int(input("Enter Student Grades"))
+# print(f" The students 3 subjects are {student_subjects} and teh gardes are {student_grades1} and {student_grades2} ,{student_grades3}")
