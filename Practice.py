c34e53b10d857cd7e9d856baef0e0eb1249de1cd
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-28 22:37:11 +0530

    Logging

--- Practice.py
+++ Practice.py
@@ -713,6 +713,11 @@ class TestAdd(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-
-
-#
+# Logging in Python
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')  
