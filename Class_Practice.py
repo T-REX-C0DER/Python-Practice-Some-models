fb7742e57373462d345bcdd74daa78fbd936d764
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-31 13:16:35 +0530

    Simple interest

--- Class_Practice.py
+++ Class_Practice.py
@@ -54,16 +54,28 @@
 # # The class problem
 # girls_count = int(input("Enter the no of girls present in class : "))
 # boys_count = int(input("Enter the no of boys present in class : "))
-
 # tubligts = ["tub1" , "tub2" , "tub3" , "tub4"]
 # fan = ["fan1" , "fan2" , "fan3" , "fan4" , "fan5" , "fan6"]
-
 # if boys_count >= 10 and girls_count >= 10 :
 #     print(f"For girls the {tubligts[2]} is on and the {fan[5]} is on")
 #     print(f"For boys the {tubligts[0]} is on and the {fan[0]} is on")
-    
 # elif boys_count >= 25 and girls_count >= 25 :
 #     print(f"For girls the {tubligts[3]} is on and the {fan[4]} is on")
 #     print(f"For boys the {tubligts[0]} is on and the {fan[3]} is on")
+
+
+# WAP to print the simple interest with bonus of a employee
+employee_name = input("Enter Employee Name: ")
+principal_amount = float(input("Enter the Principal Amount: ")) 
+rate_of_interest = float(input("Enter the Rate of Interest (in %): "))
+time_period = float(input("Enter the Time Period (in years): "))
+bonus_percentage = float(input("Enter the Bonus Percentage (in %): "))
+simple_interest = (principal_amount * rate_of_interest * time_period) / 100
+bonus_amount = (simple_interest * bonus_percentage) / 100
+total_amount = principal_amount + simple_interest + bonus_amount
+print(f"Employee Name: {employee_name}")
+print(f"Simple Interest: {simple_interest}")
+print(f"Bonus Amount: {bonus_amount}")
+print(f"Total Amount: {total_amount}")
     
 
